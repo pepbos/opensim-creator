be104ec563bf8f2eded7386a3367fe635c408c61
pepbos <pepijnvandenbos@gmail.com>
2024-02-29 14:23:30 +0100

    add run script

---
+++ scripts/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+WORKSPACE=$(dirname $(dirname $(realpath "$0")))
+echo "WORKSPACE = $WORKSPACE"
+
+# Uncomment for reconfiguring.
+# export OSC_BASE_BUILD_TYPE=RelWithDebInfo
+# env -C $WORKSPACE CC=clang-14 CXX=clang++-14 OSC_BUILD_CONCURRENCY=$(nproc) ./scripts/build_debian-buster.sh
+
+env -C $WORKSPACE cmake --build osc-build -j$(nproc)
+
+env -C $WORKSPACE/src ln -sf ../osc-build/compile_commands.json compile_commands.json
+
+env -C $WORKSPACE ./osc-build/apps/osc/osc
